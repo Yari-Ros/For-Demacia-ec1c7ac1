874eb42b543c1c7608ff640cb0d47003ab73391e
Yari-Ros <2118088@talnet.nl>
2019-11-21 10:33:06 +0100

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,14 @@
+<?php 
+
+$klas = array(
+    "Youssef",
+    "Kenza",
+    "Yari",
+    "Beau",
+    "Riccardo",
+    "George",
+    "Yari (opnieuw want leuk)",
+);
+for ($i=0; $i < count($klas) ; $i++) {
+    echo ($klas[$i] . PHP_EOL);
+} 
